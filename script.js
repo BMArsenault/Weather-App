0d6eec0dfc7d8823ca963202ab184796f87be356
Brian <BMArsenault@gmail.com>
2021-11-10 13:42:37 -0700

    Added style to css, functions to js

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
-var urlStart = "http://api.openweathermap.org/";
-var endPoint = "data/2.5/weather?q=";
-var APIkey = "&APPID=712d985d18bace0530a352f906aeff07";
+// var cityFormEl = document.querySelector("#city-form");
+// var cityInputEl = document.querySelector("#cityname");
+// var todayWeatherEl = document.querySelector("#weather");
+// var fiveDayEl = document.querySelector("#weather-icons");
+// var searchedCities = document.querySelector("#searched-cities");
+
+
+// var formSubmitHandler = function(event) {
+//     event.preventDefault();
+
+//     var cityname = cityInputEl.value.trim();
+
+//     if (cityname) {
+//         getCityWeather(cityname);
+  
+//     }
+// }
+
+function getWeatherForecast(city) {
+    var website = "http://api.openweathermap.org";
+    var APIKey = "712d985d18bace0530a352f906aeff07";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    let showResults = fetch(queryURL);
+
+
+    fetch(queryURL)
+        .then(function (response) {
+            if (response.ok) {
+            response.json();
+        }})
+        .then(showResults)
+
+}
+
+// get 5 day weather forecast for cards
+// function getCityWeather(result) {
+    
+// }
+
+
+// create new cards for 5-day forecast according to acceptance criteria
+// var card = document.createElement("div")
+// 
+
+// var date = document.createElement("div")
+// date.textContent = 
+
+// var weatherConditions = document.createElement("img")
+
+
+// var temp = document.createElement("div")
+// temp.textContent =
+
+// var wind = document.createElement("div")
+// wind.textContent = 
+
+// var humidity = document.createElement("div")
+// humidity.textContent
